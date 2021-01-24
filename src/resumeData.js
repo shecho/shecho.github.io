451aa4cbb2d8212c12a8d6e271dab5cc0805c8c4
shecho <shechovala@gmail.com>
2021-01-24 00:44:48 -0500

    some updates

--- src/resumeData.js
+++ src/resumeData.js
@@ -1,10 +1,10 @@
 let resumeData = {
     "imagebaseurl":"https://rbhatia46.github.io/",
     "name": "Sergio Valencia",
-    "role": "Frontend Developer Student",
+    "role": "Jr Frontend Developer",
     "linkedinId":"https://www.linkedin.com/in/sergio-valencia-aguirre-2a327a7a/",
     "skypeid": "",
-    "roleDescription": "I like frontend development and like to learn about new technologies, in my free time i like to drink cofee and play chest.",
+    "roleDescription": " I like frontend development and I love to learn about new technologies, in my free time I like to spend time with my kids to drink cofee and play chest.",
     "socialLinks":[
         {
           "name":"linkedin",
@@ -22,10 +22,17 @@ let resumeData = {
           "className":"fa fa-facebook"
         }
       ],
-    "aboutme":"I am currently a final year student at SENA in Information Technology. I am a self taught Full Stack Web Developer, currently diving deeper into Frontent frameworks/libraries like React, VUE Agular/IONIC. I believe that to be successful in life, you dont need talent, just need to work hard.",
+    "aboutme":"I am a self taught Full Stack Web Developer, currently diving deeper into Frontent frameworks/libraries like React, VUE Agular/IONIC. I believe that to be successful in life, you dont need talent, just need to work hard.",
     "address":"Colombia",
     "website":"",
     "education":[
+      {
+        "UniversityName":"ACADEMLO",
+        "specialization":"Desarrollo Web Full-Stacky Ciencias de la Computación",
+        "MonthOfPassing":"",
+        "YearOfPassing":"2021",
+        "Achievements":""
+      },
       {
         "UniversityName":"SENA",
         "specialization":"Administración y desarrolo de Sistemas Informáticos",
@@ -43,10 +50,17 @@ let resumeData = {
     ],
     "work":[
       {
+        "CompanyName":"Intergrupo",
+        "specialization":"Ingeniero de Desarrollo",
+        "MonthOfLeaving":"",
+        "YearOfLeaving":"Current",
+        "Achievements":""
+      },
+      {
         "CompanyName":"CWTI",
         "specialization":"Frontend Web Developer",
-        "MonthOfLeaving":"Current",
-        "YearOfLeaving":"",
+        "MonthOfLeaving":"Jul",
+        "YearOfLeaving":"2020",
         "Achievements":""
       },
       {
@@ -57,23 +71,30 @@ let resumeData = {
         "Achievements":""
       }
     ],
-    "skillsDescription":"Your skills here",
+    "skillsDescription":"Some other technologies: Mongo, sequalize, postgres, MySQL, Firebase, git, Azure Devops, Scrum, kamban, Linux, Vim",
     "skills":[
       {
-        "skillname":"HTML5"
+        "skillname":"React"
       },
       {
         "skillname":"CSS"
       },
       {
-        "skillname":"Vue(Learnning)"
+        "skillname":"Node"
+      }, 
+      {
+        "skillname":"XamarinForms"
       },
       {
-        "skillname":"Agnular"
+        "skillname":"Laravel"
       },
-      
+       {
+        "skillname":"Angular"
+      },{
+        "skillname":"Ionic"
+      },
       {
-        "skillname":"React"
+        "skillname":"Django"
       }
 
     ],
