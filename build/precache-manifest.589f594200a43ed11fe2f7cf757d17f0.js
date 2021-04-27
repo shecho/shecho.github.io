27862301d0095f9cfad938d70dc779af8dbdb64b
shecho <shechovala@gmail.com>
2021-04-26 21:11:38 -0500

    some changes

---
+++ build/precache-manifest.589f594200a43ed11fe2f7cf757d17f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5193f840d5ea301de5811247abe78028",
+    "url": "/shecho.github.io/index.html"
+  },
+  {
+    "revision": "b25393f6a2cbf0493485",
+    "url": "/shecho.github.io/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "1699fa43513f73585abc",
+    "url": "/shecho.github.io/static/js/2.1a1d116f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/shecho.github.io/static/js/2.1a1d116f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b25393f6a2cbf0493485",
+    "url": "/shecho.github.io/static/js/main.b942aaae.chunk.js"
+  },
+  {
+    "revision": "38559e40e6b783df2416",
+    "url": "/shecho.github.io/static/js/runtime-main.61c3262d.js"
+  }
+]);
